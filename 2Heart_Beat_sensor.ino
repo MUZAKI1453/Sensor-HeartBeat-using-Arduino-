1e251f606d28d2362e144dc3c902ebc064750746
Muhamad Arif Muzaki <124240121+MUZAKI1453@users.noreply.github.com>
2024-10-17 13:06:33 +0300

    Add files via upload

---
+++ 2Heart_Beat_sensor.ino
@@ -0,0 +1,27 @@
+// Variables
+int PulseSensorPurplePin = A0;
+int LED13 = 13;
+
+int Signal;
+int Threshold = 550;
+
+void setup() {
+
+  pinMode(LED13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Signal = analogRead(PulseSensorPurplePin);
+
+  Serial.println(Signal);
+
+  if (Signal > Threshold) {
+    digitalWrite(LED13, HIGH);
+  } else {
+    digitalWrite(LED13, LOW);
+  }
+
+  delay(100);
+}
+
